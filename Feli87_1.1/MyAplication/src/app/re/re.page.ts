b4ddff7f827c8444c95d2efa408941eb324a2d55
FelixAstudillo <149970825+FelixAstudillo@users.noreply.github.com>
2024-09-01 23:41:10 -0400

    Actualización 1.1
    Puse la base para las paginas de LE y RE

---
+++ Feli87_1.1/MyAplication/src/app/re/re.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-re',
+  templateUrl: './re.page.html',
+  styleUrls: ['./re.page.scss'],
+})
+export class RePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
